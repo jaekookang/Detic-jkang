24a96b528b1cc781615ff4c487a41bd09c73a4ec
jaekookang <jaekoo.jk@gmail.co>
2023-01-20 09:40:57 +0900

    minor update before migration

--- detic/modeling/utils.py
+++ detic/modeling/utils.py
@@ -4,8 +4,14 @@ import json
 import numpy as np
 from torch.nn import functional as F
 
+import os
+DETIC_DIR = '/home/jkang/project/Detic-jkang' # 220529 jkang added
+
 def load_class_freq(
-    path='datasets/metadata/lvis_v1_train_cat_info.json', freq_weight=1.0):
+    path='datasets/metadata/lvis_v1_train_cat_info.json', 
+    freq_weight=1.0):
+    
+    path = os.path.join(DETIC_DIR, path)
     cat_info = json.load(open(path, 'r'))
     cat_info = torch.tensor(
         [c['image_count'] for c in sorted(cat_info, key=lambda x: x['id'])])
